14222ea3d76a438ca3fb35d1f6ad60c4ac479eda
Steve Wozniak <jj@MacBook-Pro.local>
2016-11-13 20:37:45 +0100

    paumé

--- test.exs
+++ test.exs
@@ -9,3 +9,33 @@ defmodule Solution do
 end
 
 IO.puts Solution.remove("!Hi !!")
+
+defmodule Pipeline do
+  def fix_pipe([head | tail]) do
+  if (List.first tail) != nil and (head + 1) != hd tail do
+    # head = head ++ [(hd head) + 1]
+    IO.inspect head
+    [ tl(head) | fix_pipe(tail)]
+  else
+    [(head+1)   | fix_pipe(tail)]
+  end
+  end
+
+  def fix_pipe([]) do
+    []
+  end
+end
+
+IO.puts Pipeline.fix_pipe([2, 4, 6])
+
+defmodule Math do
+  def double_each([head | tail]) do
+    [head * 2 | double_each(tail)]
+  end
+
+  def double_each([]) do
+    []
+  end
+end
+
+IO.puts Math.double_each([1, 2, 3])
