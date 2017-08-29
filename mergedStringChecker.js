59feb41b13a935e1a2fefc4909332693f6412fdd
Jean-Jérôme Pantalacci <jean-jerome.pantalacci-ext@fnac.com>
2017-08-29 12:05:50 +0200

    final

--- mergedStringChecker.js
+++ mergedStringChecker.js
@@ -1,44 +1,36 @@
 function isMerge(s, part1, part2) {
-    var canFindInPart2 = false
     p1 = part1.split('')
     p2 = part2.split('')
-    if (p1.length + p2.length < s.length) return false
-    toto: for (let i = 0; i < s.length; i++) {
-        for (var j = 0; j < 1; j++) {
-            if (p2[j] === p1[j]) {
-                if (p2[j + 1] === s[i + 1]) {
-                    p2.shift();
-                    console.log(s[i])
-                    continue toto;
-                }
-                else if (p1[j + 1] === s[i + 1]) {
-                    p1.shift();
-                    console.log(s[i])
-                    continue toto
-                }
-            }
-            if (p2[j] === s[i]) {
-                p2.shift();
-                console.log(s[i])
-                continue toto;
-            }
+    if (p1.length + p2.length != s.length) return false
+    for (let i = 0; i < s.length; i++) {
+        // if (p2[0] === p1[0]) {
+        //     if (p2[1] === s[i + 1]) {
+        //         p2.shift();
+        //         continue;
+        //     }
+        //     else if (p1[1] === s[i + 1]) {
+        //         p1.shift();
+        //         continue
+        //     }
+        // }
+        if (p2[0] === p1[0] && p2[1] === s[i + 1] && p2[0] !== s[i] || p2[0] === s[i] && (p2[0] !== p1[0] || p1[1] !== s[i + 1])) {
+            p2.shift();
+            continue
+        }
 
-            if (p1[j] === s[i]) {
-                p1.shift();
-                console.log(s[i])
-                continue toto
-            }
+        if (p2[0] === p1[0] && p1[1] === s[i + 1] && p1[0] !== s[i]|| p1[0] === s[i]) {
+            p1.shift();
         }
     }
     return (p1.length == 0 && p2.length == 0)
 }
 
-console.log(isMerge("IHveIG_/,!aI)D*oNJ&-", "IHveG/)oNJ", "I_,!aID*&-"))
+// console.log(isMerge("IHveIG_/,!aI)D*oNJ&-", "IHveG/)oNJ", "I_,!aID*&-"))
 
-console.log(isMerge('codewars', 'code', 'wasr'));
+// console.log(isMerge('codewars', 'code', 'wasr'));
 // console.log(isMerge('codewars', 'cdw', 'oears'));
 // console.log(isMerge("Making progress", "Mak pross", "inggre"))
-// console.log(isMerge("Bananas from Bahamas", "Bahas", "Bananas from am")) //true
+console.log(isMerge("Bananas from Bahamas", "Bahas", "Bananas from am")) //true
 // console.log(isMerge('codewars', 'cod', 'wars')); //false
 // console.log(isMerge('codewars', 'code', 'code')); // false
 // console.log(isMerge('codewars', 'codes', 'wars')); // false
