fbf0dbc52694fc685c4e557e89d7711ef179e4dc
Jean-Jérôme Pantalacci <jean-jerome.pantalacci-ext@fnac.com>
2017-10-17 17:29:24 +0200

    decode dans factorial

--- factorial.js
+++ factorial.js
@@ -82,21 +82,38 @@ function longPow(n) {
     return result
 }
 
+// decode = function (w) {
+//     var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,? -"
+//     var res = ""
+//     for (let i = 0; i < w.length; i++) {
+//         var newAlpha = [];
+//         for (let j = 0; j < alphabet.length; j++) {
+//             newAlpha[((j + 1) * longPow(i+1) - 1) % alphabet.length] = j
+//         }
+//         console.log(alphabet.indexOf(w.charAt(i)))
+//         res += alphabet.charAt(newAlpha[alphabet.indexOf(w.charAt(i))])
+//     }
+//     return res;
+// }
+
 decode = function (w) {
+    //console.log(w)
     var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,? -"
     var res = ""
-    for (let i = 0; i < w.length; i++) {
+    for (let i = 1; i <= w.length; i++) {
         var newAlpha = [];
-        for (let j = 0; j < alphabet.length; j++) {
-            newAlpha[((j + 1) * longPow(i+1) - 1) % alphabet.length] = j
+        var newIndex = alphabet.indexOf(w.charAt(i - 1)) + 1
+        for (let j = 1; j <= alphabet.length; j++) {
+            if (newIndex === j * Math.pow(2, i) % 67) {
+                res += alphabet.charAt(j - 1)
+                break
+            }
         }
-        console.log(alphabet.indexOf(w.charAt(i)))
-        res += alphabet.charAt(newAlpha[alphabet.indexOf(w.charAt(i))])
     }
     return res;
 }
 
-console.log(decode("pJrZkmYVxPA5oJGXUW?OR?E3MJglc5,39Cd41rh1HHsGhrERKOPWdq"))
-console.log(decode("bdhpF,82QsLirJejtNmzZKgnB3SwTyXG ?.6YIcflxVC5WE94UA1OoD70MkvRuPqHabdh"))
+//console.log(decode("pJrZkmYVxPA5oJGXUW?OR?E3MJglc5,39Cd41rh1HHsGhrERKOPWdq"))
+//console.log(decode("bdhpF,82QsLirJejtNmzZKgnB3SwTyXG ?.6YIcflxVC5WE94UA1OoD70MkvRuPqHabdh"))
 
-console.log(decode("ZKgnB"))
+console.log(decode("zJF-CZXBFglEWVNXUR?CQWg0YUCVxhW6UCdQBu7fOaMW"))
