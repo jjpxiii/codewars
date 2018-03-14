f8588e6aadb425163b588708d1385a795177aa38
Jean-Jérôme Pantalacci <jean-jerome.pantalacci-ext@fnac.com>
2018-03-14 13:26:27 +0100

    Added some python katas

--- python/peopleinthebus.py
+++ python/peopleinthebus.py
@@ -12,3 +12,20 @@ def number(bus_stops):
 
 
 print(number([[10, 0], [3, 5], [5, 8]]))
+
+def is_square(n):
+    return n > 0 and n**(1/2) % 1 == 0    
+
+def number_to_string(n):
+    return str(n)
+
+# def validate_pin(pin):
+#     result = len(pin) == 4 or len(pin) == 6
+#     try:
+#         toto = int(pin)
+#         return result and toto >= 0
+#     except ValueError:
+#         return False
+
+def validate_pin(pin):
+    return len(pin) in (4, 6) and pin.isdigit()
