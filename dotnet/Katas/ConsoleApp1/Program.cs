2826520de6ff8af03f84897767e431f223bffa9c
Jean-Jérôme Pantalacci <jjpxiii@gmail.com>
2019-11-26 16:49:49 +0100

    Added DotNet

---
+++ dotnet/Katas/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class ConnectFour
+    {
+        public static string WhoIsWinner(List<string> piecesPositionList)
+        {
+            return "";
+            // retrun "Red" or "Yellow" or "Draw"
+        }
+    }
+}
