8080856bd4b68d95b969865270da3ec1191d86d1
jjpxiii <jjpxiii@github.com>
2020-03-22 19:15:44 +0100

    Added the Lift

---
+++ dotnet/ConsoleApp1/Dinglemouse.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Dinglemouse
+{
+    class Dinglemouse
+    {
+        public static int[] TheLift(int[][] queues, int capacity)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(queues));
+            Console.WriteLine(capacity);
+            var res = new int[] { 0 };
+            int peopleInTheLift = 0;
+            int[] liftQueues = new int[queues.Length];
+            var isGoingUp = true;
+            // lift loop
+            while (queues.ToList().Any(l => l.Length > 0) || peopleInTheLift > 0)
+            {
+                for (int level = isGoingUp ? 0 : queues.Length - 1; (isGoingUp ? level < queues.Length : level >= 0); level = (isGoingUp ? level + 1 : level - 1))
+                {
+                    var hasStopped = false;
+                    // empty lift
+                    if (peopleInTheLift > 0 && liftQueues[level] > 0)
+                    {
+                        hasStopped = true;
+                        peopleInTheLift -= liftQueues[level];
+                        liftQueues[level] = 0;
+                    }
+
+                    // fill lift
+                    if (queues[level].Any(p => isGoingUp ? p > level : p < level))
+                    {
+                        hasStopped = true;
+                        while (peopleInTheLift < capacity && queues[level].Any(p => isGoingUp ? p > level : p < level))
+                        {
+                            peopleInTheLift++;
+                            var nearestLevelToStop = queues[level].First(p => isGoingUp ? p > level : p < level);
+                            liftQueues[nearestLevelToStop]++;
+                            var foo = queues[level].ToList();
+                            foo.Remove(nearestLevelToStop);
+                            queues[level] = foo.ToArray();
+                        }
+                    }
+
+                    if (hasStopped && res.Last() != level)
+                    {
+                        Array.Resize(ref res, res.Length + 1);
+                        res[res.Length - 1] = level;
+                    }
+                }
+                isGoingUp = !isGoingUp;
+            }
+
+            if (res.Last() != 0)
+            {
+                Array.Resize(ref res, res.Length + 1);
+                res[res.Length - 1] = 0;
+            }
+            return res;
+        }
+    }
+}
---
+++ dotnet/ConsoleApp1/Dinglemouse.test.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+
+namespace Dinglemouse
+{
+
+    public class LiftTests
+    {
+        [Test]
+        public void TestUp()
+        {
+            int[][] queues =
+            {
+                new int[0], // G
+                new int[0], // 1
+                new int[]{5,5,5}, // 2
+                new int[0], // 3
+                new int[0], // 4
+                new int[0], // 5
+                new int[0], // 6
+            };
+            var result = Dinglemouse.TheLift(queues, 5);
+            Assert.AreEqual(new[] { 0, 2, 5, 0 }, result);
+        }
+
+        [Test]
+        public void TestUpOnGF()
+        {
+            int[][] queues =
+            {
+                new int[]{1,2,3,4}, // G
+                new int[0], // 1
+                new int[0], // 2
+                new int[0], // 3
+                new int[0], // 4
+                new int[0], // 5
+                new int[0], // 6
+            };
+            var result = Dinglemouse.TheLift(queues, 5);
+            Assert.AreEqual(new[] { 0, 1, 2, 3, 4, 0 }, result);
+        }
+
+        [Test]
+        public void TestCapa()
+        {
+            int[][] queues =
+            {
+                new int[0], // G
+                new int[0], // 1
+                new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1}, // 2
+                new int[0], // 3
+                new int[0], // 4
+                new int[0], // 5
+                new int[0], // 6
+            };
+            var result = Dinglemouse.TheLift(queues, 5);
+            Assert.AreEqual(new[] { 0, 2, 1, 0 }, result);
+        }
+
+        [Test]
+        public void TestDown()
+        {
+            int[][] queues =
+            {
+                new int[0], // G
+                new int[0], // 1
+                new int[]{1,1}, // 2
+                new int[0], // 3
+                new int[0], // 4
+                new int[0], // 5
+                new int[0], // 6
+            };
+            var result = Dinglemouse.TheLift(queues, 5);
+            Assert.AreEqual(new[] { 0, 2, 1, 0 }, result);
+        }
+
+        [Test]
+        public void TestUpAndUp()
+        {
+            int[][] queues =
+            {
+                new int[0], // G
+                new int[]{3}, // 1
+                new int[]{4}, // 2
+                new int[0], // 3
+                new int[]{5}, // 4
+                new int[0], // 5
+                new int[0], // 6
+            };
+            var result = Dinglemouse.TheLift(queues, 5);
+            Assert.AreEqual(new[] { 0, 1, 2, 3, 4, 5, 0 }, result);
+        }
+
+        [Test]
+        public void TestTricky()
+        {
+            int[][] queues =
+            {
+                new int[0], // G
+                new int[]{0,0,0,6}, // 1
+                new int[0], // 2
+                new int[0], // 3
+                new int[0], // 4
+                new int[]{6,6,0,0,0,6}, // 5
+                new int[0], // 6
+            };
+            var result = Dinglemouse.TheLift(queues, 5);
+            Assert.AreEqual(new[] { 0, 1, 5, 6, 5, 1, 0, 1, 0 }, result) ;
+        }
+
+        [Test]
+        public void TestDownAndDown()
+        {
+            int[][] queues =
+            {
+                new int[0], // G
+                new int[]{0}, // 1
+                new int[0], // 2
+                new int[0], // 3
+                new int[]{2}, // 4
+                new int[]{3}, // 5
+                new int[0], // 6
+            };
+            var result = Dinglemouse.TheLift(queues, 5);
+            Assert.AreEqual(new[] { 0, 5, 4, 3, 2, 1, 0 }, result);
+        }
+    }
+}
