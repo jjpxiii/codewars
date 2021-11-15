651f4e9fc1e5f5ba851502972ff68bb7b6d6c3a8
Jean-Jérôme Pantalacci <ext.jean-jerome.pantalacci@fdj-gs.com>
2021-11-15 18:40:12 +0100

    Add Tribonacci

---
+++ dotnet/Katas/Tribonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+
+    public class Tribonacci
+    {
+        public double[] Compute(double[] s, int n)
+        {
+            double[] res = new double[n];
+            Array.Copy(s, res, Math.Min(3, n));
+
+            for (int i = 3; i < n; i++)
+                res[i] = res[i - 3] + res[i - 2] + res[i - 1];
+
+            return n == 0 ? new double[] { 0 } : res;
+        }
+    }
+}
