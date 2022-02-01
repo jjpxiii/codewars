44c09a912878eb92aaf89f8cac7697e0fafd3dc5
Jean-Jérôme Pantalacci <ext.jean-jerome.pantalacci@fdj-gs.com>
2022-02-01 17:35:30 +0100

    Added dotnet/AdventOfCode

---
+++ dotnet/AdventOfCode/Day2.cs
@@ -0,0 +1,17 @@
+﻿public class Day2
+{
+    public static int Main()
+    {
+        var input = @"";
+        var inputSplitted = input.Split("\r\n").Select(o => Convert.ToInt32(o)).ToList();
+        var count = 0;
+        for (int i = 1; i < inputSplitted.Count; i++)
+        {
+            if (inputSplitted[i] - inputSplitted[i - 1] > 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ dotnet/AdventOfCode/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine(Day1.Part1());
+Console.WriteLine(Day1.Part2());
+//Console.WriteLine(Day2.Main());
