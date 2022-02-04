8ec7778e499f49c249c4b551aa15d3a77ec2da58
Jean-Jérôme Pantalacci <ext.jean-jerome.pantalacci@fdj-gs.com>
2022-02-04 17:15:31 +0100

    Added day 2

--- dotnet/AdventOfCode/Program.cs
+++ dotnet/AdventOfCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine(Day1.Part1());
-Console.WriteLine(Day1.Part2());
-//Console.WriteLine(Day2.Main());
+//Console.WriteLine(Day1.Part2());
+//Console.WriteLine(Day2.Part1());
+Console.WriteLine(Day2.Part2());
