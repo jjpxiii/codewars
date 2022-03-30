637fe78fc74d3aac7cf594872ed0b878b63be1f0
jjpxiii <jjpxiii@github.com>
2022-03-30 09:42:30 +0200

    wip

--- js/twentyfour.js
+++ js/twentyfour.js
@@ -27,15 +27,18 @@ function equalTo24(args) {
           for (let m = 0; m < 6; m++) {
             lastResult[m] = calculateEverything(secondResult[l][m], element4);
             if (lastResult[m].indexOf(24) > -1) {
-              let op = ["+", "-", "*", "/", "+", "*"];
-              // console.log("second");
-              // console.log(secondResult[l][m]);
-              // console.log("LAST");
-              // console.log(lastResult[m]);
-              // console.log(l);
-              // console.log(m);
-              // console.log(lastResult[m].indexOf(24));
+              let op = ["+", "-", "*", "/", "-", "/"];
+              console.log("first");
+              console.log(firstResult);
+              console.log("seconde");
+              console.log(secondResult[l]);
+              console.log("LAST");
+              console.log(lastResult[m]);
+              console.log(l);
+              console.log(m);
+              console.log(lastResult[m].indexOf(24));
               return addParentheses(
+                false,
                 m < 3 ? element1 : element2,
                 m < 3 ? element2 : element1,
                 element3,
@@ -68,12 +71,12 @@ function equalTo24(args) {
               intermediary[l],
               firstResult[m]
             );
-            // console.log("lastResult");
-            // console.log(lastResult[m]);
             if (lastResult[m].indexOf(24) > -1) {
+              console.log("inter");
+              console.log(intermediary);
               // console.log("ICI !");
-              // console.log("second");
-              // console.log(secondResult[l][m]);
+              console.log("first");
+              console.log(firstResult);
               console.log("LAST");
               console.log(lastResult[m]);
               console.log(m);
@@ -81,11 +84,13 @@ function equalTo24(args) {
               console.log(l);
               let op = ["+", "-", "*", "/", "-", "/"];
               let n = lastResult[m].indexOf(24);
+
               return addParentheses(
+                n === 4 || n === 5 ? true : false,
                 m > 3 ? element1 : element2,
                 m > 3 ? element2 : element1,
-                element3,
-                element4,
+                (n === 4 || n === 5) && m > 3 ? element3 : element4,
+                (n === 4 || n === 5) && m > 3 ? element4 : element3,
                 op[m],
                 op[n],
                 op[l]
@@ -99,38 +104,40 @@ function equalTo24(args) {
   return "It's not possible!";
 }
 
-function addParentheses(e1, e2, e3, e4, op1, op2, op3) {
+function addParentheses(nop = false, e1, e2, e3, e4, op1, op2, op3) {
   let p1 = "",
     p2 = "",
     p3 = "",
     p4 = "",
     p5 = "",
     p6 = "";
-  if (
-    (op1 === "*" || op1 === "/") &&
-    (op2 === "+" || op2 === "-") &&
-    (op3 === "+" || op3 === "-")
-  ) {
-    p3 = "(";
-    p4 = ")";
-  }
-  if (
-    (op3 === "*" || op3 === "/") &&
-    (op2 === "+" || op2 === "-") &&
-    (op1 === "+" || op1 === "-")
-  ) {
-    p1 = "(";
-    p2 = ")";
-  }
-  if (
-    (op3 === "-" || op3 === "+") &&
-    (op2 === "*" || op2 === "/") &&
-    (op1 === "-" || op1 === "+")
-  ) {
-    p1 = "(";
-    p5 = ")";
-    p6 = "(";
-    p4 = ")";
+  if (!nop) {
+    if (
+      (op1 === "*" || op1 === "/") &&
+      (op2 === "+" || op2 === "-") &&
+      (op3 === "+" || op3 === "-")
+    ) {
+      p3 = "(";
+      p4 = ")";
+    }
+    if (
+      (op3 === "*" || op3 === "/") &&
+      (op2 === "+" || op2 === "-") &&
+      (op1 === "+" || op1 === "-")
+    ) {
+      p1 = "(";
+      p2 = ")";
+    }
+    if (
+      (op3 === "-" || op3 === "+") &&
+      (op2 === "*" || op2 === "/") &&
+      (op1 === "-" || op1 === "+")
+    ) {
+      p1 = "(";
+      p5 = ")";
+      p6 = "(";
+      p4 = ")";
+    }
   }
   return `${p1}${e1}${op1}${p3}${e2}${p5}${op2}${p6}${e3}${p2}${op3}${e4}${p4}`;
 }
@@ -141,8 +148,8 @@ function calculateEverything(a, b) {
 // console.log(equalTo24(1, 2, 3, 4));
 // console.log(equalTo24(2, 3, 4, 5));
 // console.log(equalTo24(3, 4, 5, 6));
-// console.log(equalTo24(1, 1, 13, 1));
-// console.log(equalTo24(2, 7, 7, 13));
-console.log(equalTo24(13, 13, 6, 12));
+// console.log(equalTo24(1, 1, 1, 13));
+console.log(equalTo24(2, 7, 7, 13));
+// console.log(equalTo24(13, 13, 6, 12));
 // console.log(equalTo24(13, 13, 13, 13));
 module.exports = equalTo24;
--- js/twentyfour.test.js
+++ js/twentyfour.test.js
@@ -6,8 +6,11 @@ describe("Basic tests", () => {
     expect(equalTo24(1, 2, 3, 4)).toBe("(1+2+3)*4");
     expect(equalTo24(2, 3, 4, 5)).toBe("(3-2+5)*4");
     expect(equalTo24(3, 4, 5, 6)).toBe("(3-4+5)*6");
-    expect(equalTo24(1, 1, 13, 1)).toBe("(1+1)*(13-1)");
+    expect(equalTo24(1, 1, 1, 13)).toBe("(1+1)*(13-1)");
     expect(equalTo24(2, 7, 7, 13)).toBe("7/7+13*2");
+    expect(equalTo24(13, 13, 6, 12)).toBe("13+13-12/6");
+    expect(equalTo24(2, 7, 7, 13)).toBe("2*(13-(7/7))");
+    expect(equalTo24(4, 3, 1, 6)).toBe("6/(1-(3/4))");
     expect(equalTo24(13, 13, 13, 13)).toBe("It's not possible!");
   });
 });
